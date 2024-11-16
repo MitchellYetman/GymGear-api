d66803caabbb4d5a80713b2aeec2af917a141f04
Mitchell Yetman <w0498497@nscc.ca>
2024-11-15 21:24:45 -0400

    added database schema and ran migration

---
+++ SERVER/prisma/migrations/20241116012417_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "customer_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "customer_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "cost" DECIMAL NOT NULL,
+    "filename" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
