60bd3f4363f1a8a264577ae5bf6f4e1fe7cca12b
Mitchell Yetman <w0498497@nscc.ca>
2024-12-04 21:29:01 -0400

    logout page

--- client/src/routes/Logout.jsx
+++ client/src/routes/Logout.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
 export default function Logout() {
+    const [status, setStatus] = useState("Logging out...");
+
+    useEffect(() => {
+        async function logout() {
+            const url = "http://localhost:3000/api/customers/logout";
+            const response = await fetch(url, {
+                method: "POST",
+                credentials: 'include' //include cookies in request
+            });
+
+            if (response.ok) {
+                setStatus("Successfully logged out");
+            } else {
+                setStatus("Error encountered. Try again")
+            }
+        }
+
+        logout();
+    }, []);
     return (
         <>
-            <div>
-                <h1>Logout page</h1>
-            </div>
+            <h1>Logout</h1>
+            <p>{status}</p>
+            <Link to="/login" className="btn btn-outline-secondary ms-3">Login</Link>
+            <Link to="/" className="btn btn-outline-secondary ms-3">Return to homepage</Link>
         </>
     )
 }
